f4b9a3a5f5a968e1ac90fe3782ebb574ab32be43
unknown <maxgabriel2005@yahoo.com>
2022-06-17 19:04:49 +0300

    Curs 1

---
+++ src/curs1/Tester.java
@@ -0,0 +1,36 @@
+package curs1;
+
+public class Tester {
+
+	
+	 // --> one line comment
+	/*
+	 * multiline comment
+	 */
+	/**
+	 *  -->javadoc
+	 */
+	
+	//variabile
+	public String name;
+	public int ratePerHour;
+	
+	//metode
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {  //obj.setName("Oana")
+		this.name = name;
+	}
+	public int getRatePerHour() {
+		return ratePerHour;
+	}
+	public void setRatePerHour(int ratePerHour) {
+		this.ratePerHour = ratePerHour;
+	}
+	
+	
+	 
+	 
+	
+}
---
+++ src/curs1/wagecalculator.java
@@ -0,0 +1,37 @@
+package curs1;
+
+public class wagecalculator {
+	
+	public int hoursWorked = 40;
+
+	public static void main(String[] args) {
+		
+		Tester tester1 = new Tester();
+		tester1.setName("Oana");
+		tester1.setRatePerHour(55);
+		
+		Tester tester2 = new Tester();
+		
+	    tester2.setName("Matei");
+	    tester2.setRatePerHour(30);
+		System.out.println(tester2.getName());
+		System.out.println(tester1.getName());
+		
+		wagecalculator Obj = new wagecalculator ();
+		System.out.println(Obj.calculateSalary(tester1.getRatePerHour()));
+				
+		
+		
+	  
+		
+	}
+	
+	public int calculateSalary(int plataPeOra) {
+		
+		int salary = plataPeOra*hoursWorked;
+		return salary;
+	}
+	
+	
+
+}
